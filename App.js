2ac097ed62aa39aba0148749a6dbb4090a225a10
Rodrigo Polastro ESCOLA <rodrigo.polastro03@gmail.com>
2023-10-23 09:32:25 -0300

    query full address by cep and display it on the screen

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView
+} from 'react-native'
+import apiCep from './services/apiCep'
 
 export default function App() {
+  const [searchCep, setSearchCep] = useState('')
+  const [address, setAddress] = useState({})
+
+  //try it with the fetch api and see if it works
+  const getAdressByCep = async () => {
+    const { data } = await apiCep.get(`${searchCep}/json/`)
+    console.log(data)
+    setAddress(data)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.screen}>
+      <Text> Busca CEP </Text>
+      <TextInput
+        value={searchCep}
+        onChangeText={text => setSearchCep(text)}
+        style={styles.cepInput}
+        placeholder='Informe o CEP'
+        keyboardType='numeric'
+      />
+      <TouchableOpacity style={styles.searchButton} onPress={getAdressByCep}>
+        <Text style={styles.searchButtonText}> Buscar Endereço</Text>
+      </TouchableOpacity>
+      <View>
+        <Text>{address.logradouro}</Text>
+        <Text>Bairro: {address.bairro}</Text>
+        <Text>Cidade: {address.localidade}</Text>
+        <Text>UF: {address.uf}</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'skyblue',
+    padding: 20,
+    paddingTop: 60,
   },
-});
+  cepInput: {
+    backgroundColor: 'white'
+  },
+  searchButton: {
+    backgroundColor: 'blue',
+  },
+  searchButtonText: {
+    color: 'white',
+  }
+})
